2e2dc2f3571e911fd625e621a2eb40c333313932
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2026-01-29 15:40:05 -0600

    final

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("Oct 25, 2026 17:00:00").getTime(),
+let countDown = new Date("Oct 24, 2026 17:00:00").getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
